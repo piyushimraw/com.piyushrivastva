09e571d3fb6535115cd35f3e05841976429ef8de
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-06-02 13:55:50 +0530

    added gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,8 +26,29 @@ export default class MyDocument extends Document {
               `,
             }}
           ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K4N28SV');</script>
+              `,
+            }}
+          ></script>
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-K4N28SV"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+              name="gtag"
+              title="gtag"
+            />
+          </noscript>
           <Main />
           <NextScript />
         </body>
