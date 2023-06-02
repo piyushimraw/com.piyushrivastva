6dd3f9608a716f9ecbdeea6dc5b415fbf38a4220
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-06-02 13:50:58 +0530

    added tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,6 @@
-import { useEffect } from "react";
 import { AppProps } from "next/app";
 
 import "../components/styles/layout.css";
-import TagManager from "react-gtm-module";
-import ANALYTICS_ID from "../lib/gtag";
 
 /**
  * Root level component for all pages
@@ -11,9 +8,6 @@ import ANALYTICS_ID from "../lib/gtag";
  * @param {object} pageProps All props for the page
  */
 function MyApp({ Component, pageProps }: AppProps) {
-  useEffect(() => {
-    TagManager.initialize({ gtmId: ANALYTICS_ID });
-  }, []);
   return <Component {...pageProps} />;
 }
 
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,10 +8,24 @@ export default class MyDocument extends Document {
       <Html lang="en-us">
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-WXG0KCPWNX"
+          ></script>
           <link
             href={`https://unpkg.com/prismjs@0.0.1/themes/prism-okaidia.css`}
             rel="stylesheet"
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-WXG0KCPWNX');
+              `,
+            }}
+          ></script>
         </Head>
         <body>
           <Main />
