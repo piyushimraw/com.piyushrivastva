751f56adad17930d6b8c033ef7f8ed7797ce0890
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-06-12 21:31:41 +0530

    added menu in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,6 +5,7 @@ import { styles } from "@/constant/styles";
 import { navLinks } from "@/constant/content";
 import { logo, menu, close } from "../assets";
 import Image from "next/image";
+import clsx from "clsx";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
@@ -13,11 +14,15 @@ const Navbar = () => {
     window.scrollTo(0, 0);
   };
 
+  const onMenuClick = (title: string) => {
+    setActive(title);
+  };
+
   return (
     <nav
-      className={`${styles.paddingX} flex items-center py-5 fixed top-0 z-20 bg-primary`}
+      className={`${styles.paddingX} flex items-center py-5 fixed top-0 z-20 bg-primary w-full`}
     >
-      <div className="flex items-center justify-between w-full max-w-7xl mx-auto">
+      <div className="flex justify-between items-center w-full max-w-7xl mx-auto">
         <Link
           onClick={onLinkClick}
           href="/"
@@ -30,7 +35,34 @@ const Navbar = () => {
             height={40}
             className="w-9 h-9 object-contain "
           />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Piyush
+          </p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link, index) => (
+            <li
+              className={clsx(
+                "hover:text-white",
+                link.title === active ? "text-white" : "text-secondary",
+                "text-[18px]",
+                "font-medium",
+                "cursor-pointer"
+              )}
+              key={link.id}
+            >
+              <Link
+                onClick={() => onMenuClick(link.title)}
+                href={`#${link.id}`}
+              >
+                {link.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <Image src={menu} alt="menu" width={30} height={30} />
+        </div>
       </div>
     </nav>
   );
