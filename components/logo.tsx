5f37dbab2ac545b288954bc41117e76e4466e263
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-06-12 19:37:11 +0530

    added initial files

--- components/logo.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { StyledLogo } from "./styles/logo.styles";
-
-const Logo = () => {
-  return <StyledLogo src="/images/logo.svg" alt="piyushrivastva.tech logo" />;
-};
-
-export default Logo;
