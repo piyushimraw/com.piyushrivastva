5ce56455b7d8dcd39858154c426f1f36cb695e29
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-06-16 03:38:44 +0530

    remove sw

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
  */
 const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: true,
 });
 
 const nextConfig = {
