e384332aa7adc96d90b9f7903956bd4e15026711
Piyush Shrivastava <piyush.shrivastava@mindtickle.com>
2023-06-15 22:32:34 +0530

    fixed issue with extra tailwind classes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,12 +17,12 @@ const Hero = () => {
 
         <div>
           <h1
-            className={`font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-lg lg:leading-[98px] mt-2 text-white`}
+            className={`font-black text-white lg:text-[80px] sm:text-[60px] xs:text-[50px] text-lg lg:leading-[98px] mt-2`}
           >
             Hi, I&rsquo;m <span className="text-[#915EFF]">Piyush</span>
           </h1>
           <p
-            className={`text-[#dfd9ff] font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100`}
+            className={`font-medium lg:text-[30px] sm:text-[26px] xs:text-[20px] text-[16px] lg:leading-[40px] mt-2 text-white-100`}
           >
             I am a full stack developer and I develop{" "}
             <br className="hidden sm:block" />
